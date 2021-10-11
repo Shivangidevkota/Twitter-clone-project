37b825689838e1c5197b91250366d12990b62774
Musubi Management <musubimanagement@Musubis-MacBook-Air-10.local>
2021-10-11 15:06:16 +0530

    deploying

--- django_forumm/settings.py
+++ django_forumm/settings.py
@@ -19,7 +19,6 @@ import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -157,3 +156,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
